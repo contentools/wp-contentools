8f6fc0c5edf75846ff63d47eab813148fc4e84cd
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2016-07-21 17:46:49 -0300

    Add SVn structure

---
+++ trunk/wp-contentools.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: WP Contentools
+Plugin URI: https://github.com/contentools/wp-contentools
+Description: Contentools is a content marketing automation platform to centralize, streamline and manage content creation. This plugin enables the integration between the Contentools Platform and Wordpress.
+Author: Contentools
+Version: 1.0
+Author URI: http://contentools.com/
+License: GPLv2+
+*/
+
+remove_action( 'login_init', 'send_frame_options_header', 10, 0 );
+remove_action( 'admin_init', 'send_frame_options_header', 10, 0 );
+
+add_action( 'login_init', 'WPContentools_send_origin_frame_options_header', 10, 0 );
+add_action( 'admin_init', 'WPContentools_send_origin_frame_options_header', 10, 0 );
+
+add_action( 'send_headers', 'WPContentools_set_enaled_plugin' );
+
+function WPContentools_send_origin_frame_options_header() {
+    $origin = 'http://go.contentools.com/';
+    @header( 'X-Frame-Options: ' . $origin );
+}
+
+function WPContentools_set_enaled_plugin() {
+    @header( 'X-WP-Contentools: true' );
+}
+
+add_action( 'init', 'WPContentools_allow_origin' );
+function WPContentools_allow_origin() {
+   @header("Access-Control-Allow-Origin: *");
+   @header("Access-Control-Expose-Headers: X-WP-Contentools");
+}
+
+?>
